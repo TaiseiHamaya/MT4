7decdce82e4345b936dde242c33e860fd0b1cb47
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2024-12-11 15:07:27 +0900

    MT4_01_01_確認課題

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <Novice.h>
 
-const char kWindowTitle[] = "学籍番号";
+#include "Math/Quaternion.h"
+
+const char kWindowTitle[] = "LE2A_16_ハマヤ_タイセイ";
+
+void PrintMatrix4x4(int x, int y, const Matrix4x4& matrix) {
+	static const int kRowHeight = 20;
+	static const int kColumnWidth = 60;
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(x + column * kColumnWidth, y + row * kRowHeight, "% 4.3f", matrix[row][column]);
+		}
+	}
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -11,6 +23,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	Vector3 axis = Vector3{ 1.0f, 1.0f, 1.0f }.normalize();
+	Quaternion quaternion = Quaternion::AngleAxis(axis, 0.44f);
+	Matrix4x4 matrix = quaternion.to_matrix();
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -32,6 +48,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		PrintMatrix4x4(0, 0, matrix);
 
 		///
 		/// ↑描画処理ここまで
